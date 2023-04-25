1a27b398e29fde4d893c85264f7a3b0ee72f5f7e
giwankim <giwan.kim@gmail.com>
2023-04-26 03:40:40 +0900

    0단계 - 뼈대 코드 준비하기 (#33)
    * feat: add racing car
    
    * feat: add racing car console application

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class RacingCar {
+  private final String name;
+  private int position;
+
+  public RacingCar(String name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public void move(int randomNumber) {
+    if (randomNumber >= 4) {
+      this.position += 1;
+    }
+  }
+}
