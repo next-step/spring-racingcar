4be1135bb3a35f326625bacab48d8dcb35e41805
chacha <peony@bluehole.net>
2023-04-27 00:06:01 +0900

    feat Spring에서 racing car에 대한 정보를 request하는 class 추가

---
+++ src/main/java/racingcar/dto/RacingCarRequestDto.java
@@ -0,0 +1,14 @@
+package racingcar.dto;
+
+public class RacingCarRequestDto {
+    String names;
+    int count;
+
+    public String getNames() {
+        return names;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
