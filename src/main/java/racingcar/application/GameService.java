05eebb19e12ca0c4b8822911079b5667904c83db
hjhello423 <hjhello423@gmail.com>
2023-04-27 22:04:39 +0900

    feat: service에 db에 저장하도록 변경

--- src/main/java/racingcar/application/GameService.java
+++ src/main/java/racingcar/application/GameService.java
@@ -1,6 +1,8 @@
 package racingcar.application;
 
 import org.springframework.stereotype.Service;
+import racingcar.domain.PlayHistoryRepository;
+import racingcar.domain.PlayResultRepository;
 import racingcar.domain.RacingCars;
 import racingcar.domain.RacingGame;
 import racingcar.domain.RandomMoveStrategy;
@@ -10,6 +12,14 @@ import racingcar.presentation.dto.PlayResponse;
 @Service
 public class GameService {
 
+    private final PlayResultRepository resultRepository;
+    private final PlayHistoryRepository historyRepository;
+
+    public GameService(PlayResultRepository resultRepository, PlayHistoryRepository historyRepository) {
+        this.resultRepository = resultRepository;
+        this.historyRepository = historyRepository;
+    }
+
     public PlayResponse play(PlayRequest request) {
         RacingCars racingCars = RacingCars.of(request.getPlayers());
         int playCount = request.getCount();
@@ -17,6 +27,9 @@ public class GameService {
         RacingGame racingGame = RacingGame.of(racingCars);
         racingGame.play(playCount, new RandomMoveStrategy());
         String winners = racingGame.getWinners();
+
+        resultRepository.save(winners, playCount);
+        historyRepository.save(racingCars);
 
         return PlayResponse.of(winners, racingCars);
     }
