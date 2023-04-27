c8ca6c2cdf7b57787aaf9abd4eb20bef4c0b8706
hjhello423 <hjhello423@gmail.com>
2023-04-27 22:31:52 +0900

    fix: 커밋 꼬인 부분 반영

--- src/main/java/racingcar/infra/PlayResultDao.java
+++ src/main/java/racingcar/infra/PlayResultDao.java
@@ -1,7 +1,11 @@
 package racingcar.infra;
 
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import racingcar.domain.PlayResultRepository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
 
 @Repository
 public class PlayResultDao implements PlayResultRepository {
@@ -10,22 +14,22 @@ public class PlayResultDao implements PlayResultRepository {
     private static final String ID = "id";
     private static final String[] COLUMNS = {"winners", "trial_count"};
 
-//    private final SimpleJdbcInsert simpleJdbcInsert;
-//
-//    public PlayResultDao(DataSource dataSource) {
-//        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
-//                .withTableName(TABLE_NAME)
-//                .usingGeneratedKeyColumns(ID)
-//                .usingColumns(COLUMNS);
-//    }
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public PlayResultDao(DataSource dataSource) {
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName(TABLE_NAME)
+                .usingGeneratedKeyColumns(ID)
+                .usingColumns(COLUMNS);
+    }
 
     @Override
     public void save(String winners, int trialCount) {
-//        HashMap<String, Object> param = new HashMap<>();
-//        param.put("winners", winners);
-//        param.put("trial_count", trialCount);
-//
-//        simpleJdbcInsert.execute(param);
+        HashMap<String, Object> param = new HashMap<>();
+        param.put("winners", winners);
+        param.put("trial_count", trialCount);
+
+        simpleJdbcInsert.execute(param);
     }
 
 }
