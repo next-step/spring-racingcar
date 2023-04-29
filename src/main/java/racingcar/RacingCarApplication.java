29e5984c36e5bc0be618d454fbb85cd35f6eeda1
rladowl92 <rladowl92@gmail.com>
2023-04-29 19:14:45 +0900

    feat: racingcar 뼈대코드 추가

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,13 +1,28 @@
 package racingcar;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import racingcar.domain.Cars;
+import racingcar.domain.RandomState;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
-@SpringBootApplication
+import java.util.List;
+
 public class RacingCarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RacingCarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        List<String> carNames = inputView.inputCarNames();
+        int numberOfAttempts = inputView.inputNumberOfAttempts();
 
+        OutputView outputView = new OutputView();
+        outputView.printResultMessage();
+
+        Cars cars = new Cars(carNames);
+        for (int i = 0; i < numberOfAttempts; i++) {
+            cars.attempt(new RandomState());
+            outputView.printStatusFromScores(cars.getCars());
+        }
+
+        outputView.printWinners(cars.getWinners());
+    }
 }
