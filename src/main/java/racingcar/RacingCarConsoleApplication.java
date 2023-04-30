49f363ce1d8c15337b42f290bce211063d20e88b
ujusy <lsyeon1997@ajou.ac.kr>
2023-04-30 22:09:19 +0900

    add: RacingCarService 구현

--- src/main/java/racingcar/RacingCarConsoleApplication.java
+++ src/main/java/racingcar/RacingCarConsoleApplication.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import racingcar.domain.RacingCar;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
---
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -0,0 +1,33 @@
+package racingcar.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import racingcar.controller.dto.request.PlayRacingCarRequest;
+import racingcar.controller.dto.response.PlayRacingCarResponse;
+import racingcar.domain.RacingCarGame;
+import racingcar.domain.RacingCars;
+import racingcar.repository.PlayResultRepository;
+
+@Service
+public class RacingCarService {
+
+    private final PlayResultRepository playResultRepository;
+
+    @Autowired
+    public RacingCarService(PlayResultRepository playResultRepository) {
+        this.playResultRepository = playResultRepository;
+    }
+
+    public PlayRacingCarResponse play(PlayRacingCarRequest request) {
+        RacingCars racingCars = RacingCars.of(request.getPlayerNames());
+
+        RacingCarGame racingCarGame = RacingCarGame.of(racingCars, request.getCount());
+        racingCarGame.play();
+
+        String winners = racingCarGame.winners();
+
+        playResultRepository.save(winners);
+
+        return PlayRacingCarResponse.of(winners, racingCars);
+    }
+}
