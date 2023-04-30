70b80699d87043a8f085d1704df24ada9c1b3d9b
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-30 12:02:37 +0900

    RacingCar.java domain 디렉토리로 이동

--- src/main/java/racingcar/web/controller/GameController.java
+++ src/main/java/racingcar/web/controller/GameController.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import racingcar.RacingCar;
+import racingcar.domain.RacingCar;
 import racingcar.web.dto.PlaysRequest;
 
 @Slf4j
