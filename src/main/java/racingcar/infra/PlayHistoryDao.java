9650948993f35681ba94435fe3d0521e6d76cb80
hjhello423 <hjhello423@gmail.com>
2023-05-01 00:11:08 +0900

    feat: rowMapper 추가, 게임 이력 조회 메서드 추가

--- src/main/java/racingcar/infra/PlayHistoryDao.java
+++ src/main/java/racingcar/infra/PlayHistoryDao.java
@@ -1,24 +1,28 @@
 package racingcar.infra;
 
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import racingcar.domain.PlayHistory;
 import racingcar.domain.PlayHistoryRepository;
+import racingcar.infra.rowmaper.PlayHistoryRwoMapper;
 
 import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
-public class PlayHistoryDao implements PlayHistoryRepository {
+public class PlayHistoryDao extends NamedParameterJdbcDaoSupport implements PlayHistoryRepository {
 
     private static final String TABLE_NAME = "player_history";
     private static final String ID = "id";
     private static final String[] COLUMNS = {"play_result_id", "name", "position"};
+    private static final PlayHistoryRwoMapper ROW_MAPPER = new PlayHistoryRwoMapper();
 
     private final SimpleJdbcInsert simpleJdbcInsert;
 
     public PlayHistoryDao(DataSource dataSource) {
+        super.setDataSource(dataSource);
         this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
                 .withTableName(TABLE_NAME)
                 .usingGeneratedKeyColumns(ID)
@@ -38,6 +42,13 @@ public class PlayHistoryDao implements PlayHistoryRepository {
                         .addValue("name", it.getName())
                         .addValue("position", it.getPosition()))
                 .toArray(MapSqlParameterSource[]::new);
+    }
+
+    @Override
+    public List<PlayHistory> findAll() {
+        final String query = String.format("SELECT * FROM %s", TABLE_NAME);
+        return getNamedParameterJdbcTemplate()
+                .query(query, ROW_MAPPER);
     }
 
 }
