15d0ab3235208386ec6f4b885bf1aaa5d8858060
hjhello423 <hjhello423@gmail.com>
2023-05-01 00:12:11 +0900

    test: 테스트 격리 처리

--- src/test/java/racingcar/fixture/AssuredTest.java
+++ src/test/java/racingcar/fixture/AssuredTest.java
@@ -4,8 +4,11 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestExecutionListeners;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestExecutionListeners(value = {DatabaseCleanup.class,}, mergeMode =
+        TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 public class AssuredTest {
 
     @LocalServerPort
