7a169980f7b6dbf4bf7dbc56fc50c5b49640dc26
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-30 15:20:42 +0900

    PlayResult, 리포지토리 - 롬복 어노테이션 추가

--- src/main/java/racingcar/domain/PlayResult.java
+++ src/main/java/racingcar/domain/PlayResult.java
@@ -1,9 +1,17 @@
 package racingcar.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import racingcar.domain.dto.RacingGameResult;
 
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PlayResult {
     private int id;
     private int trialCount;
--- src/main/java/racingcar/domain/repository/PlayResultRepository.java
+++ src/main/java/racingcar/domain/repository/PlayResultRepository.java
@@ -3,7 +3,6 @@ package racingcar.domain.repository;
 import org.springframework.stereotype.Repository;
 import racingcar.domain.PlayResult;
 
-@Repository
 public interface PlayResultRepository {
 
     void save(PlayResult playResult);
