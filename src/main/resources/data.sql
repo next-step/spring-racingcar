0ef159fb14b801e2ecee705fb002bd4032fee20c
JangHyeonJun2 <epfvkdlxj@naver.com>
2023-04-30 21:45:47 +0900

    create: 2-1. 테이블 설계

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,15 @@
 CREATE TABLE PLAY_RESULT (
     id          INT         NOT NULL AUTO_INCREMENT,
     winners     VARCHAR(50) NOT NULL,
+    trial_count INT         NOT NULL,
     created_at  DATETIME    NOT NULL default current_timestamp,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE PLAYER_FINAL_TRAVEL_DISTANCE (
+    id          INT         NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(20) NOT NULL ,
+    position    INT         NOT NULL ,
+    created_at  DATETIME    NOT NULL default current_timestamp,
+    PRIMARY KEY (id)
+)
