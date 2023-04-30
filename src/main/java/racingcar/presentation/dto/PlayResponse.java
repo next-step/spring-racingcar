bde7434ce6d977792a274a84f90a5d2a9e0d2b77
hjhello423 <hjhello423@gmail.com>
2023-05-01 00:11:34 +0900

    feat: 게임 이력 응답 dto 정의

--- src/main/java/racingcar/presentation/dto/PlayResponse.java
+++ src/main/java/racingcar/presentation/dto/PlayResponse.java
@@ -1,5 +1,7 @@
 package racingcar.presentation.dto;
 
+import racingcar.domain.PlayHistory;
+import racingcar.domain.PlayResult;
 import racingcar.domain.RacingCar;
 import racingcar.domain.RacingCars;
 
@@ -30,6 +32,13 @@ public class PlayResponse {
         return new PlayResponse(winners, carDtos);
     }
 
+    public static PlayResponse of(PlayResult playResult, List<PlayHistory> histories) {
+        String winners = playResult.getWinners();
+        List<RacingCarDto> racingCarDtos = histories.stream()
+                .map(RacingCarDto::new)
+                .collect(Collectors.toList());
+        return new PlayResponse(winners, racingCarDtos);
+    }
 
     public String getWinners() {
         return winners;
@@ -46,6 +55,11 @@ public class PlayResponse {
         public RacingCarDto(RacingCar racingCar) {
             this.name = racingCar.getName();
             this.position = racingCar.getPosition();
+        }
+
+        public RacingCarDto(PlayHistory playHistory) {
+            this.name = playHistory.getName();
+            this.position = playHistory.getPosition();
         }
 
         public String getName() {
