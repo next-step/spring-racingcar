93e9eab251e0f041eaf5041f54f36b610406db2f
ujusy <lsyeon1997@ajou.ac.kr>
2023-04-30 22:09:47 +0900

    add: 웹으로 요청 받을 수 있는 RacingCarController 구현

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,28 @@
+package racingcar.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import racingcar.controller.dto.request.PlayRacingCarRequest;
+import racingcar.controller.dto.response.PlayRacingCarResponse;
+import racingcar.service.RacingCarService;
+
+@RestController
+public class RacingCarController {
+
+    private final RacingCarService racingCarService;
+
+    @Autowired
+    public RacingCarController(RacingCarService racingCarService) {
+        this.racingCarService = racingCarService;
+    }
+
+    @PostMapping("/plays")
+    public ResponseEntity<PlayRacingCarResponse> play(@RequestBody PlayRacingCarRequest request) {
+        PlayRacingCarResponse response = racingCarService.play(request);
+
+        return ResponseEntity.ok(response);
+    }
+}
