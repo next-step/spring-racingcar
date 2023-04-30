e87ca26380332ec7fdc84168ac54e2ab5037941b
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-30 14:14:26 +0900

    GameController plays로직 구현, 반환 DTO 추가

--- src/main/java/racingcar/web/controller/GameController.java
+++ src/main/java/racingcar/web/controller/GameController.java
@@ -1,5 +1,6 @@
 package racingcar.web.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -7,18 +8,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import racingcar.domain.RacingCar;
+import racingcar.domain.dto.RacingGameResult;
+import racingcar.domain.service.RacingCarService;
 import racingcar.web.dto.PlaysRequest;
+import racingcar.web.dto.PlaysResponse;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class GameController {
+
+    private final RacingCarService racingCarService;
 
     @PostMapping(value = "/plays",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public RacingCar plays(@RequestBody PlaysRequest playsRequest) {
+    public PlaysResponse plays(@RequestBody PlaysRequest playsRequest) {
         log.info(playsRequest.toString());
-        return null;
+        RacingGameResult gameResult = racingCarService.plays(playsRequest.getNames(),
+                playsRequest.getCount());
+
+        return PlaysResponse.from(gameResult);
     }
 }
