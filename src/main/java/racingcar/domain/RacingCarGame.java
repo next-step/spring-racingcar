1c4dde17d475dbe9604009b5ee6f0cc4c6137d20
ujusy <lsyeon1997@ajou.ac.kr>
2023-04-30 22:08:20 +0900

    add: 게임 진행 로직 RacingCarGame 추가

---
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -0,0 +1,54 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RacingCarGame {
+
+    private final RacingCars racingCars;
+
+    private final Integer count;
+
+    private RacingCarGame(RacingCars racingCars, Integer count) {
+        this.racingCars = racingCars;
+        this.count = count;
+    }
+
+    public static RacingCarGame of(RacingCars racingCars, Integer count) {
+        return new RacingCarGame(racingCars, count);
+    }
+
+    public void play() {
+        for (int i = 0; i < count; i++) {
+            playRound(racingCars);
+        }
+    }
+
+    public String winners() {
+        int maxPosition = 0;
+        
+        List<String> winners = new ArrayList<>();
+
+        for (RacingCar racingCar : racingCars.getRacingCars()) {
+            if (racingCar.getPosition() > maxPosition) {
+                maxPosition = racingCar.getPosition();
+                winners.clear();
+            }
+
+            if (racingCar.getPosition() >= maxPosition) {
+                winners.add(racingCar.getName());
+            }
+        }
+
+        return String.join(", ", winners);
+    }
+
+    private void playRound(RacingCars racingCars) {
+        Random random = new Random();
+        for (RacingCar racingCar : racingCars.getRacingCars()) {
+            int randomNumber = random.nextInt(10);
+            racingCar.move(randomNumber);
+        }
+    }
+}
