9fc2bb38528d43c686aab13164cc0a4eca0be62e
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-30 12:01:18 +0900

    PlayRequest dto, add getRacingCar()

--- src/main/java/racingcar/web/dto/PlaysRequest.java
+++ src/main/java/racingcar/web/dto/PlaysRequest.java
@@ -1,6 +1,11 @@
 package racingcar.web.dto;
 
 import lombok.*;
+import racingcar.domain.RacingCar;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -14,5 +19,15 @@ public class PlaysRequest {
     @Override
     public String toString() {
         return "names : " + names + " count : " + count;
+    }
+
+    public List<RacingCar> getRacingCars() {
+        return Arrays.stream(this.getNameArray())
+                .map(it -> new RacingCar(it.trim()))
+                .collect(Collectors.toList());
+    }
+
+    private String[] getNameArray() {
+        return names.split(",");
     }
 }
