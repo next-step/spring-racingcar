607eb87aa50d4cc8d6557fb5923aa89d5422b13f
Yonghwi Kim <yh0921k@gmail.com>
2023-05-03 00:14:54 +0900

    feat: 자동차 리스트에 대한 일급 컬렉션 구현
    RacingCars.java
    - Names 일급 컬렉션으로 가공된 사용자 요청 필드(자동차 이름)를 전달받아 자동차 리스트에 대한 일급 컬렉션 생성
    - 승리한 차량의 이름을 조회하는 기능 구현

---
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -0,0 +1,41 @@
+package racingcar.domain;
+
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class RacingCars {
+  private final List<RacingCar> values;
+
+  public RacingCars(Names names) {
+    this.values = generate(names);
+  }
+
+  private List<RacingCar> generate(Names names) {
+    return names.getValues().stream().map(RacingCar::new).collect(Collectors.toList());
+  }
+
+  public String getWinners() {
+    Integer maximumPosition = getPositions().getMaximumPosition();
+    return getNamesBy(maximumPosition);
+  }
+
+  private Positions getPositions() {
+    List<Integer> positions =
+        this.values.stream().map(RacingCar::getPosition).collect(Collectors.toList());
+
+    return new Positions(positions);
+  }
+
+  private String getNamesBy(Integer maximumPosition) {
+    List<String> winners =
+        this.values.stream()
+            .filter(car -> car.getPosition() == maximumPosition)
+            .map(RacingCar::getName)
+            .collect(Collectors.toList());
+
+    return String.join(",", winners.toArray(String[]::new));
+  }
+}
