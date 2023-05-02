b9f00c9a7e2fa713abf2dfa3a622113a60f9c7ff
Yonghwi Kim <yh0921k@gmail.com>
2023-05-03 02:26:42 +0900

    feat: 자동차 경주에 API에 대해 구현된 쿼리 적용
    RacingCarController.java
    - 초기 서비스 레이어에 자동차 리스트 및 게임 횟수를 전달했으나, 데이터베이스 테이블 구조상 요청 파라미터인 이름을 그대로 저장할 필요가 있어 메서드 스펙 변경
    
    RacingCarService.java
    - 초기 게임 히스토리 데이터 저장
    - 차량 인스턴스 생성
    - 배치 쿼리를 위한 데이터 리스트 생성
    - 게임 라운드 진행
    - 게임 라운드 히스토리 데이터 저장(배치)
    - 우승자 조회 및 데이터 업데이트
    - 결과 반환

--- src/main/java/racingcar/application/RacingCarService.java
+++ src/main/java/racingcar/application/RacingCarService.java
@@ -1,25 +1,52 @@
 package racingcar.application;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import racingcar.domain.MovementPolicy;
+import racingcar.domain.Names;
 import racingcar.domain.RacingCars;
+import racingcar.domain.repository.RacingCarRepository;
 import racingcar.dto.RacingCarNamePosition;
 import racingcar.dto.RacingCarPlayResponse;
+import racingcar.dto.RacingCarRoundResult;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class RacingCarService {
-  public RacingCarPlayResponse play(RacingCars cars, int count) {
+
+  private final RacingCarRepository racingCarRepository;
+
+  @Transactional
+  public RacingCarPlayResponse play(String names, int count) {
+    // 초기 게임 히스토리 데이터 저장
+    Integer gameId = this.racingCarRepository.saveGameHistory(names, count);
+
+    // 차량 인스턴스 생성
+    RacingCars cars = new RacingCars(new Names(names));
+
+    // 배치 쿼리를 위한 데이터 리스트 생성
+    List<RacingCarRoundResult> roundResults = new LinkedList<>();
+
     // 게임 라운드 진행
     MovementPolicy movementPolicy = new MovementPolicy();
-    for (int i = 0; i < count; i++) {
+    for (int i = 1; i <= count; i++) {
       playEachRound(cars, movementPolicy);
+      roundResults.addAll(createRoundResults(gameId, i, cars));
     }
+
+    // 게임 라운드 히스토리 데이터 저장
+    this.racingCarRepository.saveRoundHistory(roundResults);
 
     // 우승자 조회
     String winners = cars.getWinners();
+
+    // 우승자 데이터 업데이트
+    this.racingCarRepository.updateWinners(gameId, winners);
 
     // 모든 차에 대한 경기 결과
     List<RacingCarNamePosition> racingCarNamePositions =
@@ -40,5 +67,19 @@ public class RacingCarService {
 
   private void playEachRound(RacingCars cars, MovementPolicy movementPolicy) {
     cars.getValues().forEach(car -> car.move(movementPolicy.action()));
+  }
+
+  private List<RacingCarRoundResult> createRoundResults(
+      Integer gameId, int count, RacingCars cars) {
+    return cars.getValues().stream()
+        .map(
+            car ->
+                RacingCarRoundResult.builder()
+                    .gameId(gameId)
+                    .round(count)
+                    .carName(car.getName())
+                    .position(car.getPosition())
+                    .build())
+        .collect(Collectors.toList());
   }
 }
--- src/main/java/racingcar/presentation/RacingCarController.java
+++ src/main/java/racingcar/presentation/RacingCarController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import racingcar.application.RacingCarService;
-import racingcar.domain.Names;
-import racingcar.domain.RacingCars;
 import racingcar.dto.RacingCarPlayRequest;
 import racingcar.dto.RacingCarPlayResponse;
 
@@ -20,8 +18,7 @@ public class RacingCarController {
   @PostMapping("/plays")
   public ResponseEntity<RacingCarPlayResponse> play(@RequestBody RacingCarPlayRequest request) {
     RacingCarPlayResponse response =
-        this.racingCarService.play(
-            new RacingCars(new Names(request.getNames())), request.getCount());
+        this.racingCarService.play(request.getNames(), request.getCount());
 
     return ResponseEntity.ok(response);
   }
