0b321982094311b1b32e561371e9aaf05934fd07
chacha <peony@bluehole.net>
2023-05-04 01:52:24 +0900

    refactor 메소드명 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,49 +4,48 @@ import racingcar.behavior.MovingStrategy;
 import racingcar.util.ErrorMessage;
 
 public class Car {
-    static final int DEFAULT_DISTANCE = 0;
-    private static final int MAX_INPUT_NAME_VALUE = 5;
-    private static final String BLANK = " ";
+	static final int DEFAULT_DISTANCE = 0;
+	private static final int MAX_INPUT_NAME_VALUE = 5;
+	private static final String BLANK = " ";
 
-    private final String name;
-    private int position;
+	private final String name;
+	private int position;
 
-    public Car(String name) {
-        this.position = DEFAULT_DISTANCE;
-        this.name = validateNamesOfCars(name);
-    }
+	public Car(String name) {
+		this.position = DEFAULT_DISTANCE;
+		this.name = validateNamesOfCar(name);
+	}
 
-    public static String validateNamesOfCars(String name) {
-        if (isNullOrEmpty(name) || isFiveLetterWords(name)) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_NAME_ERROR);
-        }
-        return name;
-    }
+	public static String validateNamesOfCar(String name) {
+		if (isNullOrEmpty(name) || isFiveLetterWords(name)) {
+			throw new IllegalArgumentException(ErrorMessage.INPUT_NAME_ERROR);
+		}
+		return name;
+	}
 
-    static boolean isNullOrEmpty(String name) {
-        return name.trim().isEmpty() || name.equals(BLANK);
-    }
+	static boolean isNullOrEmpty(String name) {
+		return name.trim().isEmpty() || name.equals(BLANK);
+	}
 
-    static boolean isFiveLetterWords(String carName) {
-        return carName.trim().length() > MAX_INPUT_NAME_VALUE;
-    }
+	static boolean isFiveLetterWords(String carName) {
+		return carName.trim().length() > MAX_INPUT_NAME_VALUE;
+	}
 
-    public void move(final MovingStrategy movingStrategy) {
-        if (movingStrategy.isMovable()) {
-            position++;
-        }
-    }
+	public void move(final MovingStrategy movingStrategy) {
+		if (movingStrategy.isMovable()) {
+			position++;
+		}
+	}
 
-    public int getPosition() {
-        return this.position;
-    }
+	public int getPosition() {
+		return this.position;
+	}
 
-    public boolean hasSamePosition(int position) {
-        return this.position == position;
-    }
+	public boolean hasSamePosition(int position) {
+		return this.position == position;
+	}
 
-
-    public String getName() {
+	public String getName() {
 		return name;
 	}
 }
