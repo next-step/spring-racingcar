a1bb14762dd7899d29e9aef33c10fa3355d20ca1
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-03 20:48:20 +0900

    remove(web): unused import

--- src/main/java/racingcar/web/dto/PlaysRequest.java
+++ src/main/java/racingcar/web/dto/PlaysRequest.java
@@ -1,14 +1,9 @@
 package racingcar.web.dto;
 
 import lombok.*;
-import racingcar.domain.RacingCar;
-
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
