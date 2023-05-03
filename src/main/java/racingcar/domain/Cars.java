8b16107457bde7360bb1ce17d45675f6e043a8e6
gmelon <hsh1769@naver.com>
2023-05-03 22:34:03 +0900

    refactor: Cars.carList() 메서드를 private로 변경하고 생성자를 통해 테스트 하도록 변경

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import racingcar.strategy.MovingStrategy;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,8 +19,10 @@ public class Cars {
         this.cars = cars;
     }
 
-    static List<Car> carList(String[] carNames) {
-        return Stream.of(carNames).map(Car::new).collect(Collectors.toList());
+    private List<Car> carList(String[] carNames) {
+        return Stream.of(carNames)
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 
     public void move(MovingStrategy movingStrategy) {
@@ -30,4 +33,16 @@ public class Cars {
         return cars.stream().map(Car::getPlayResult).collect(Collectors.toUnmodifiableList());
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cars cars1 = (Cars) o;
+        return Objects.equals(cars, cars1.cars);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cars);
+    }
 }
