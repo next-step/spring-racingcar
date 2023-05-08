120975ceb93d33ac0f599767188f72dc1f52ef0f
road-jin <rjswmtk@gmail.com>
2023-05-08 14:04:01 +0900

    refactor: view 분리

--- src/main/java/racingcar/RacingCarConsoleApplication.java
+++ src/main/java/racingcar/RacingCarConsoleApplication.java
@@ -10,20 +10,39 @@ import racingcar.game.dto.PlayResultResponse;
 public class RacingCarConsoleApplication {
     public static void main(String[] args) {
         // 자동차 입력
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String names = scanner.nextLine();
+        String names = InputView.getRacingCarNames();
 
         // 시도 횟수 입력
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int count = scanner.nextInt();
+        int count = InputView.getCount();
 
         // 경주 시작
         GameService gameService = new GameService(new MemoryPlayResultDao(), new MemoryPlayHistoryDao());
         PlayResultResponse playResultResponse = gameService.play(new PlayRequest(names, count));
 
         // 우승자 조회
-        System.out.println();
-        System.out.println("최종 우승자: " + playResultResponse.getWinners());
+        OutputView.printGameResult(playResultResponse);
+    }
+
+    static class InputView {
+
+        private static final Scanner SCANNER = new Scanner(System.in);
+
+        public static String getRacingCarNames() {
+            System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+            return SCANNER.nextLine();
+        }
+
+        public static int getCount() {
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            return SCANNER.nextInt();
+        }
+    }
+
+    static class OutputView {
+
+        public static void printGameResult(PlayResultResponse playResultResponse) {
+            System.out.println();
+            System.out.println("최종 우승자: " + playResultResponse.getWinners());
+        }
     }
 }
