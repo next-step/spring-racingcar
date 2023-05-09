28e9c8415038e1c64a92bc0e7e9abdfa9f078bc6
chacha <peony@bluehole.net>
2023-05-10 01:08:17 +0900

    feat 내용 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
 	private static final int MIN_INPUT_VALUE = 1;
 	private static final String QUESTION_NUMBER_OF_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-	 static final String QUESTION_NUMBER_OF_ATTEMPT = "시도할 횟수는 몇 회 인가요?";
+	static final String QUESTION_NUMBER_OF_ATTEMPT = "시도할 횟수는 몇 회 인가요?";
 	private static final String INVALID_NUMBER_OF_ATTEMPT = "시도할 횟수를 최소 1회 이상 입력 하세요.";
 
 	private static final Scanner scanner = new Scanner(System.in);
