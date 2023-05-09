a40b41033e094fcce0cf4a4b744d9e5a41bef3a6
chacha <peony@bluehole.net>
2023-05-09 16:18:16 +0900

    feat MemoryGameResultDao 추가

---
+++ src/main/java/racingcar/domain/dao/MemoryGameResultDao.java
@@ -0,0 +1,35 @@
+package racingcar.domain.dao;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+import racingcar.domain.GameResult;
+import racingcar.repository.GameResultRepository;
+
+public class MemoryGameResultDao implements GameResultRepository {
+	private final Map<Long, GameResult> store;
+	private final AtomicLong id;
+
+	public MemoryGameResultDao() {
+		this.store = new ConcurrentHashMap<>();
+		this.id = new AtomicLong(1);
+	}
+
+	@Override
+	public long save(GameResult gameResult) {
+		store.put(id.get(), gameResult);
+		return id.getAndIncrement();
+	}
+
+	@Override
+	public List<GameResult> findAll() {
+		return store.values()
+			.stream()
+			.sorted(Comparator.comparing(GameResult::getId))
+			.collect(Collectors.toUnmodifiableList());
+	}
+}
