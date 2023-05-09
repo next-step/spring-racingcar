deb2753068d4235ae1026a35b0afc225cf78ae66
chacha <peony@bluehole.net>
2023-05-09 16:26:42 +0900

    feat RacingCarResponseDto로 변환시키는 메소드 추가

--- src/main/java/racingcar/dto/RacingCarResponseDto.java
+++ src/main/java/racingcar/dto/RacingCarResponseDto.java
@@ -12,4 +12,25 @@ public class RacingCarResponseDto {
 		this.winners = winners;
 		this.racingCars = racingCars;
 	}
+
+	public static RacingCarResponseDto of(GameResult gameResult, List<GameHistory> histories) {
+		String winners = gameResult.getWinners();
+		List<Car> cars = histories.stream()
+			.map(history -> new Car(history.getName(), history.getPosition()))
+			.collect(Collectors.toList());
+		return new RacingCarResponseDto(winners, cars);
+	}
+
+	public static RacingCarResponseDto from(String winners, RacingGame racingGame) {
+		List<Car> cars = racingGame.getCars();
+		return new RacingCarResponseDto(winners, cars);
+	}
+
+	public String getWinners() {
+		return winners;
+	}
+
+	public List<Car> getRacingCars() {
+		return racingCars;
+	}
 }
